49b2708c8a907dbd67a699c8ed87e70896a849ea
any22/07 <anygabrielifa@gmail.com>
2024-07-22 11:48:41 -0300

    e4t

---
+++ Fornecedor.cs
@@ -0,0 +1,15 @@
+public class Fornecedor : Pessoa
+{
+    string CPF;
+ 
+public void GetCPF ()
+{
+ return CPF;
+}
+public void SetNome (string n)
+{
+    Nome=n;
+}
+
+
+}
---
+++ Pessoa.cs
@@ -0,0 +1,43 @@
+public class Pessoa
+{
+string Nome;
+string Endereço;
+string Telefone;
+int id;
+
+public void GetNome ()
+{
+    return Nome;
+}
+public void SetNome (string n)
+(
+    Nome=n;
+)
+public void GetEndereço ()
+{
+    return Endereço;
+}
+public void SetEndereço (string e)
+{
+    return Endereço=e;
+}
+public void GetTelefone ()
+{
+    return Telefone;
+}
+public void SetTelefone (string t)
+{
+    Telefone=t;
+}
+public void GetId ()
+{
+    return id;
+}
+public void SetID (string i)
+{
+    Nome=n;
+}
+}
+
+
+
